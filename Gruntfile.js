4c1b969b624d2d6a1e74345a2ed613114f363669
Rollie Ma <rollie.ma@gmail.com>
2014-12-16 23:32:02 -0800

    Added an empty Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+/*
+ * shorten.js
+ *
+ * URL shortener backed by Node.js, Express and MongoDB.
+ * https://github.com/waltzofpearls/shorten.js
+ *
+ * Copyright (c) 2014 Topbass Labs (topbasslabs.com)
+ * Author Waltz.of.Pearls <rollie@topbasslabs.com, rollie.ma@gmail.com>
+ */
