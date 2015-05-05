7812b7e9d43506d3ae64b51078d5f2fc9afcf88f
Rollie Ma <rollie.ma@gmail.com>
2015-05-04 23:51:13 -0700

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 var argv = require('yargs').argv;
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,8 @@
-
 'use strict';
 
 var angular = require('./bower/angular/angular');
+
+angular.module('shortenJs', [
+  'ngRoute',
+  'shortenJs.version'
+]);
