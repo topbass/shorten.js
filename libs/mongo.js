342cd40dd3d03edc883baaf4a56b1bb6b560648d
Rollie Ma <rollie.ma@gmail.com>
2015-07-02 00:20:20 -0700

    Updated mongo connection lib

--- libs/mongo.js
+++ libs/mongo.js
@@ -2,14 +2,43 @@
 
 var config = require('../config/consolidate');
 var mongoose = require('mongoose');
+var util = require('util');
 
-mongoose.connect('mongodb://' + config.mongo.user + ':' + config.mongo.password + '@' + config.mongo.server +'/' + config.mongo.database);
-mongoose.connection.on('error', function (err) {
-  console.error('MongoDB error: ' + err.message);
-  console.error('Make sure a mongoDB server is running and accessible by this application');
-  process.exit(1);
+var uri = util.format(
+  'mongodb://%s:%s@%s/%s',
+  config.mongo.user,
+  onfig.mongo.password,
+  config.mongo.server,
+  config.mongo.database
+);
+
+// Create the database connection
+mongoose.connect(uri);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open to ' + dbURI);
 });
-mongoose.connection.on('open', function (err) {
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+  // console.error('Make sure a mongoDB server is running and accessible by this application');
+  // process.exit(1);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
 });
 
 module.exports = mongoose;
