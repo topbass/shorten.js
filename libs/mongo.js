c34b3fe1812fb859cbe1d3e37c5815ef9e9aeecd
Rollie Ma <rollie.ma@gmail.com>
2015-07-01 23:08:58 -0700

    Mongo connection lib

--- libs/mongo.js
+++ libs/mongo.js
@@ -1,4 +1,15 @@
 'use strict';
 
-var config = require('config');
+var config = require('../config/consolidate');
 var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://' + config.mongo.user + ':' + config.mongo.password + '@' + config.mongo.server +'/' + config.mongo.database);
+mongoose.connection.on('error', function (err) {
+  console.error('MongoDB error: ' + err.message);
+  console.error('Make sure a mongoDB server is running and accessible by this application');
+  process.exit(1);
+});
+mongoose.connection.on('open', function (err) {
+});
+
+module.exports = mongoose;
