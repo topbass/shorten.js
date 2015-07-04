e9ae64e6c4d9970c72068fb3e7633a52805d9349
Rollie Ma <rollie.ma@gmail.com>
2015-07-04 15:35:30 -0700

    Mongo connection management lib

--- libs/mongo.js
+++ libs/mongo.js
@@ -7,38 +7,47 @@ var util = require('util');
 var uri = util.format(
   'mongodb://%s:%s@%s/%s',
   config.mongo.user,
-  onfig.mongo.password,
+  config.mongo.password,
   config.mongo.server,
   config.mongo.database
 );
 
-// Create the database connection
-mongoose.connect(uri);
+var connectionStates = {
+  disconnected: 0,
+  connected: 1,
+  connecting: 2,
+  disconnecting: 3,
+};
 
-// CONNECTION EVENTS
-// When successfully connected
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose default connection open to ' + dbURI);
-});
+if (mongoose.connection.readyState === connectionStates.disconnected) {
+  // Create the database connection
+  mongoose.connect(uri);
 
-// If the connection throws an error
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose default connection error: ' + err);
-  // console.error('Make sure a mongoDB server is running and accessible by this application');
-  // process.exit(1);
-});
-
-// When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
-});
-
-// If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-  mongoose.connection.close(function () {
-    console.log('Mongoose default connection disconnected through app termination');
-    process.exit(0);
+  // CONNECTION EVENTS
+  // When successfully connected
+  mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + uri);
   });
-});
+
+  // If the connection throws an error
+  mongoose.connection.on('error',function (err) {
+    console.log('Mongoose default connection error: ' + err);
+    // console.error('Make sure a mongoDB server is running and accessible by this application');
+    // process.exit(1);
+  });
+
+  // When the connection is disconnected
+  mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+  });
+
+  // If the Node process ends, close the Mongoose connection
+  process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+      console.log('Mongoose default connection disconnected through app termination');
+      process.exit(0);
+    });
+  });
+}
 
 module.exports = mongoose;
