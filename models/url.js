4c0eb10f36cdc53168568a1681818028eb1b9949
Rollie Ma <rollie.ma@gmail.com>
2015-07-04 23:43:54 -0700

    Url collection schema

--- models/url.js
+++ models/url.js
@@ -4,7 +4,18 @@ var hashids = require('hashids');
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
+
 var Url = new Schema({
+  hash: String,
+  long_url: String,
+  status: String,
+  views: Number,
+  title, String,
+  note: String,
+  archived: Boolean,
+  timestamp: {type: Date, default: Date.now}
 });
+
+Url.index({hash: 1});
 
 module.exports = mongoose.model('Url', Url);
