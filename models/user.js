0a43654222e83f1b346f4430219e4bd594cbab4b
Rollie Ma <rollie.ma@gmail.com>
2015-07-06 23:50:09 -0700

    Added fields to User collection

--- models/user.js
+++ models/user.js
@@ -6,6 +6,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
+  apiKey: String,
+  member_since: {type: Date, default: Date.now}
+  display_name, String,
+  full_name: String
 });
 
 User.index({hash: 1});
