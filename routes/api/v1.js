491dca9cf492168e86abed6a6bf97fa9e0dcb973
Rollie Ma <rollie.ma@gmail.com>
2015-07-06 23:58:33 -0700

    Imported mongo connection lib and mongoose models to routes/api/v1.js

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -2,6 +2,10 @@
 
 var config = require('config');
 var express = require('express');
+var mongoose = require('../../libs/mongo');
+
+var Link = require('../../models/link');
+var User = require('../../models/user');
 
 var router = express.Router();
 
